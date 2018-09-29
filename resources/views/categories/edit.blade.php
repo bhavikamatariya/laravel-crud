a562ff53432f5e096c785cc1b376cf996be92568
Bhavika Matariya <bhavikapatel989@gmail.com>
2018-09-29 16:47:23 +0530

    Category CRUD

---
+++ resources/views/categories/edit.blade.php
@@ -0,0 +1,51 @@
+@extends('categories.layout')
+
+@section('content')
+    <div class="container">
+        <div class="row">
+            <div class="col-lg-12 margin-tb">
+                <div class="pull-left">
+                    <h2>Edit Category</h2>
+                </div>
+                <div class="pull-right">
+                    <a class="btn btn-primary" href="{{ route('categories.index') }}">Back</a>
+                </div>
+            </div>
+        </div>
+
+        @if ($errors->any())
+            <div class="alert alert-danger">
+                <strong>Whoops!</strong> There were some problems with your input.<br><br>
+                <ul>
+                    @foreach ($errors->all() as $error)
+                        <li>{{ $error }}</li>
+                    @endforeach
+                </ul>
+            </div>
+        @endif
+
+        <form action="{{ route('categories.update', $category->id) }}" method="POST">
+            @csrf
+
+            @method('PUT')
+
+            <div class="row">
+                <div class="col-xs-12 col-sm-12 col-md-12">
+                    <div class="form-group">
+                        <strong>Title</strong>
+                        <input type="text" name="title" value="{{ $category->title }}" placeholder="Title" class="form-control">
+                    </div>
+                </div>
+                <div class="col-xs-12 col-sm-12 col-md-12">
+                    <div class="form-group">
+                        <strong>Description</strong>
+                        <textarea name="description" placeholder="Description" class="form-control">{{ $category->description }}</textarea>
+                    </div>
+                </div>
+                <div class="col-xs-12 col-sm-12 col-md-12 text-center">
+                    <button type="submit" class="btn btn-primary"> Update</button>
+                </div>
+            </div>
+        </form>
+    </div>
+@endsection
---
+++ resources/views/categories/layout.blade.php
@@ -0,0 +1,6 @@
+@extends('layouts.app')
+
+<head>
+    <title>CRUD Demo</title>
+    <link href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.0.0-alpha/css/bootstrap.css" rel="stylesheet">
+</head>
